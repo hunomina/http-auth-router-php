4940edf46b486373d7d604ef7e837e53144e82d6
hunomina <hugo.f972@gmail.com>
2019-03-05 17:55:29 +0100

    Initial commit

---
+++ src/SecurityContext/JsonSecurityContext.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace hunomina\Routing\Auth\SecurityContext;
+
+
+class JsonSecurityContext
+{
+
+}
---
+++ src/UserInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace hunomina\Router\Auth;
+
+
+class UserInterface
+{
+
+}
